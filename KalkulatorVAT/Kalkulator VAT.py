f0b8632972ced26783756df619bc0a51cd910bb3
Dominik <devdomin@gmail.com>
2018-03-24 10:39:10 +0100

    Add files via upload

---
+++ KalkulatorVAT/Kalkulator VAT.py
@@ -0,0 +1,43 @@
+# Program Kalkulator VAT oblicza stawkę podatku w podanej kwocie
+
+
+
+def netto():
+    netto_input = float(input("Podaj kwotę Netto: "))
+    netto_vat = float((netto_input * 0.23) + netto_input)
+    sam_vat_netto = float(netto_input * 0.23)
+    print("\nKwota z podatkiem wynosi:", netto_vat, "PLN")
+    print("Podatek vat wynosi: ", sam_vat_netto)
+
+def brutto():
+    brutto_input = float(input("Podaj kwotę Brutto: "))
+    vat_brutto = float(((brutto_input / 1.23) * 0.23))
+    kwota_netto = float(brutto_input - vat_brutto)
+    print("\nPodatek vat od kwoty ", round(brutto_input, 2), "wynosi: ", round(vat_brutto, 2), "PLN")
+    print("Wartość netto dla kwoty", round(brutto_input, 2), "wynosi: ", round(kwota_netto, 2), "PLN")
+
+print("Witam w programie do liczenie podatku VAT, wybierz jaką kwotę chcesz policzyć:")
+
+choice = None
+
+while choice != "0":
+    print(
+        """
+        0 - Zakończ
+        1 - Netto
+        2 - Brutto
+        """
+        )
+    choice = input("Wybierz: ")
+    print()
+
+    if choice == "0":
+        print("Do widzenia! :)")
+
+    elif choice == "1":
+        netto()
+
+    elif choice == "2":
+        brutto()
+
+input("Aby zakończyć naciśnij Enter")
