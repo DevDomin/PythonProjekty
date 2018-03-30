987337d234683979a433ea956d459022f6639bd5
Dominik <devdomin@gmail.com>
2018-03-31 00:23:12 +0200

    Add files via upload

---
+++ ProsteProgramy/komputer_zgaduje_liczbe.py
@@ -0,0 +1,28 @@
+# Gracz wybiera liczbe z przedzialu 1-100, a komputer stara się odgadnać jaka to liczba :)
+
+
+print("Witaj użyttkowniku. Po podaniu przez Cibie liczby komputer postara się ją odgadnąć, może mu się uda :)")
+
+# Użytkownik wybiera liczbę
+
+wybrana_liczba = input("\nWybierz liczbę z przedziału od 1 do 100: ")
+
+# Zabezpiecznie przed wprowadzeniem innej wartości niż liczba lub wyższej od 100
+
+try:
+    wybrana_liczba = int(wybrana_liczba)
+
+except ValueError:
+    print("Podana wartość musi być liczbą! :)")
+    exit()
+
+if wybrana_liczba >= 100:
+    print("Wybrana liczba musi być z zakresu od 1 do 100! Nie umiesz się bawić, do widzenia.")
+
+# Komputer filtruje informację i podaje wynik
+
+y = wybrana_liczba
+for x in range(1, 101):
+    if x == y:
+        print("Komputer zgadł! To było do przewidzenia, Twoja liczba to: ", x)
+
